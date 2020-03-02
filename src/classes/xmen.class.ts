ac95e2f7760cb9664a1d6392b4bb1d6943d85eb8
mark7dev <mark7dev@hotmail.com>
2020-03-02 13:54:17 -0600

    import classes

---
+++ src/classes/xmen.class.ts
@@ -0,0 +1,12 @@
+export class Xmen {
+
+    constructor(
+        public name: string,
+        public key: string
+    ) {}
+
+    print() {
+        console.log( `${this.name} - ${this.key}` );
+        
+    }
+}
